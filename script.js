04d60f88b4c1ef90be0043c24e40068379e9695a
Gary Ditsch <garyditsch@gmail.com>
2021-11-22 18:17:39 -0500

    added progress bar, made it respond to seconds, formatted numbers to always have two digits

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 console.log('hello')
 
-let deadline = 'December 31 2022 023:59:59'
+let deadline = 'December 31 2021 023:59:59'
 
 const getCountDownTime = (deadline) => {
     const total = (new Date(deadline).getTime()) - Date.now()
-    let days = Math.floor(total / (1000 * 60 * 60 * 24));
+    let weeks = Math.floor(total / (1000 * 60 * 60 * 24 * 7));
+    if(weeks < 10){ weeks = '0' + weeks}
+    let days = Math.floor(total % (1000 * 60 * 60 * 24 * 7) / (1000 * 60 * 60 * 24));
+    if(days < 10){ days = '0' + days}
     let hours = Math.floor((total % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    if(hours < 10){ hours = '0' + hours}
     let minutes = Math.floor((total % (1000 * 60 * 60)) / (1000 * 60));
+    if(minutes < 10){ minutes = '0' + minutes}
+
     let seconds = Math.floor((total % (1000 * 60)) / 1000);
+    let totalDays = Math.floor(total / (1000 * 60 * 60 * 24));
     
     
     return {
         'total': total,
+        'weeks': weeks,
+        'totalDays': totalDays,
         'days': days,
         'hours': hours,
         'minutes': minutes,
@@ -20,14 +29,15 @@ const getCountDownTime = (deadline) => {
 }
 
 const setTimerNumber = (getCountDownTime, deadline) => {
-    const times = getCountDownTime(deadline)
+    let times = getCountDownTime(deadline)
+    // console.log(times)
 
+    document.getElementById("weeks-timer").innerHTML = times.weeks
     document.getElementById("day-timer").innerHTML = times.days
     document.getElementById("hours-timer").innerHTML = times.hours
     document.getElementById("minutes-timer").innerHTML = times.minutes
-    document.getElementById("seconds-timer").innerHTML = times.seconds
+    document.getElementById("progress-bar").value = times.seconds
+    // document.getElementById("seconds-timer").innerHTML = times.seconds
 }
 
 let resetTimer = setInterval(setTimerNumber, 1000, getCountDownTime, deadline);
-// let resettingTimer = setInterval(setTimerNumber(times), 1000)
-// setInterval(function(){ alert("Hello"); }, 3000);
