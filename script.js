3b0f5da05f4cd84bc287f4ba7f0c51bf9002f2a9
Gary Ditsch <garyditsch@gmail.com>
2022-04-09 11:49:32 -0400

    updating clock

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 console.log('hello')
 
 let deadline = 'December 31 2060 23:59:59'
+// let deadline = 'December 31 2022 23:59:59'
+// let deadline = 'September 30 2060 23:59:59'
 
 const getCountDownTime = (deadline) => {
-    const total = (new Date(deadline).getTime()) - Date.now()
-    let years = Math.floor(total / (1000 * 60 * 60 * 24 * 7 * 52.1775))
+    const offset = ((new Date(deadline)).getTimezoneOffset() * 60000)
+    const total = ((new Date(deadline).getTime()) - offset) - (Date.now() - offset)
 
-    let weeks = Math.floor(total % (1000 * 60 * 60 * 24 * 7 * 52.1775) / (1000 * 60 * 60 * 24 * 7));
-    if(weeks < 10){ weeks = '0' + weeks}
-    console.log(weeks)
-    
-    let days = Math.floor(total % (1000 * 60 * 60 * 24 * 7) / (1000 * 60 * 60 * 24));
-    if(days < 10){ days = '0' + days}
-    
-    let hours = Math.floor((total % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    if(hours < 10){ hours = '0' + hours}
-    
+    const milliYear = 31556952000
+    const milliWeek = 604800000
+    const milliDay = 86400000
+    const milliHour = 3600000
+    let years = Math.floor(total / milliYear)
+
+    let weeks = Math.floor((total - (milliYear * years)) / milliWeek)    
+    let days = Math.floor((total - (years * milliYear) - (weeks * milliWeek)) / milliDay)    
+    let hours = Math.floor((total - (years * milliYear) - (weeks * milliWeek) - (days * milliDay)) / milliHour)    
     let minutes = Math.floor((total % (1000 * 60 * 60)) / (1000 * 60));
-    if(minutes < 10){ minutes = '0' + minutes}
-
     let seconds = Math.floor((total % (1000 * 60)) / 1000);
     let totalDays = Math.floor(total / (1000 * 60 * 60 * 24));
-    
-    // console.log(total)co
+
+    if(weeks < 10){ weeks = '0' + weeks}
+    if(days < 10){ days = '0' + days}
+    if(hours < 10){ hours = '0' + hours}
+    if(minutes < 10){ minutes = '0' + minutes}
+
     return {
         'total': total,
         'years': years,
@@ -37,17 +40,12 @@ const getCountDownTime = (deadline) => {
 
 const setTimerNumber = (getCountDownTime, deadline) => {
     let times = getCountDownTime(deadline)
-    // console.log(times)
 
     document.getElementById("weeks-timer").innerHTML = times.years
     document.getElementById("day-timer").innerHTML = times.weeks
     document.getElementById("hours-timer").innerHTML = times.days
     document.getElementById("minutes-timer").innerHTML = times.hours
     document.getElementById("progress-bar").value = times.seconds
-    // document.getElementById("seconds-timer").innerHTML = times.seconds
 }
 
 let resetTimer = setInterval(setTimerNumber, 1000, getCountDownTime, deadline);
-
-// let width = window.innerWidth;
-// console.log(width)
